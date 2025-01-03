3ed4877810a83ef67d91c7668ee3d7f63be0689d
mraihaan22 <mraihaan64@gmail.com>
2025-01-04 00:09:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 require("./doc/module.js")
 
 global.prefix = ['','!','.',',','#','/']
-global.owner = ['916909137213']
-global.ownMain = '916909137213'
+global.owner = ['6285929014979']
+global.ownMain = '6285929014979'
 global.NamaOwner = '🦄드림 가이 Xeon' //
 global.sessionName = 'session'
 global.connect = true // 
